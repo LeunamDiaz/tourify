fd543d3932e2b77f311f58fc0155bfc1dcfe4516
TheSixDecadence <DelosSantosJ.10.3A@gmail.com>
2023-10-29 14:29:20 -0600

    implementacionAlertas1

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static('./'));
-
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'Tourify',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use(require('./routes/index'));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
-const path = require('path');
+
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -34,71 +34,11 @@ router.get('/', (req, res) => {
     res.render('login.ejs');
 });
 
-router.post('/home', (req, res) => {
-    const email = req.body.email;
-    const enteredPassword = req.body.password;
-
-    const loginSQL = 'SELECT * FROM users WHERE email = ?';
-    db.query(loginSQL, [email], (error, results) => {
-        if (error) {
-            console.error('Error al consultar la base de datos:', error.message);
-            res.status(500).json({ error: 'Error de servidor' });
-        } else {
-            if (results.length === 0) {
-                // Usuario no encontrado
-                res.send('Datos incorrectos.');
-            } else {
-                // Usuario encontrado, continúa con el flujo de la aplicación
-                const storedPasswordHash = results[0].password;
-                bcrypt.compare(enteredPassword, storedPasswordHash, (error, passwordMatch) => {
-                    if (error) {
-                        console.error('Error al comparar contraseñas:', error);
-                        res.status(500).json({ error: 'Error de servidor' });
-                    } else if (passwordMatch) {
-                        // Contraseña coincidente, el usuario está autenticado con éxito
-                        res.render('index.ejs');
-                    } else {
-                        // Contraseña incorrecta
-                        console.log('Contraseña incorrecta')
-                        res.send('Datos incorrectos.');
-                    }
-                });
-            } 
-        }
-    });
-});
-
 router.get('/register', (req, res) => {
     res.render('register.ejs');
 });
 
-router.post('/register', (req, res) => {
-    const name = req.body.name;
-    const password = req.body.password;
-    const email = req.body.email;
-
-    // Genera un hash de la contraseña
-    bcrypt.hash(password, 10, (error, hashedPassword) => {
-        if (error) {
-            console.error('Error al generar el hash de la contraseña:', error);
-            res.status(500).json({ error: 'Error de servidor' });
-        } else {
-            const registerSQL = 'INSERT INTO users(name, email, password) VALUES (?,?,?)';
-            const valores = [name, email, hashedPassword]; // Almacena el hash en lugar de la contraseña en texto claro
-            db.query(registerSQL, valores, (error, resultado) => {
-                if (error) {
-                    console.error('No se pudo insertar el dato', error.message);
-                    res.status(500).json({ error: 'Error de servidor' });
-                } else {
-                    console.log('Datos insertados correctamente');
-                    res.sendFile('login.ejs');
-                }
-            });
-        }
-    });
-});
-
-router.get('/home', (req, res) => {
+router.get('/homeu', (req, res) => {
     res.render('index.ejs');
 });
 
@@ -110,8 +50,52 @@ router.get('/achievements', (req, res) => {
     res.render('achievements.ejs')
 });
 
+router.get('/profile', (req, res) => {
+    res.render('profile.ejs')
+});
+
 router.get('/admin', (req, res) => {
     res.render('admin.ejs')
-})
+});
+
+router.post('/register', (req, res) => {
+    const name = req.body.name;
+    const password = req.body.password;
+    const email = req.body.email;
+    const registerSQL = 'INSERT INTO users(name, email, password) VALUES (?,?,?)';
+    const valores = [name, email, password];
+    db.query(registerSQL, valores, (error, resultado) => {
+        if(error){
+            console.error('No se pudo insertar el dato'+error.message);
+        }
+        console.log('Datos insertados correctamente');
+        res.redirect('/');
+    });
+});
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const loginSQL = 'SELECT * FROM users WHERE email = ? AND password = ?'
+    const valores = [email, password];
+    db.query(loginSQL, valores, (error, resultado) => {
+        if (error) {
+            console.error('Error al consultar la base de datos:', error.message);
+            // Manejar el error apropiadamente
+            res.status(500).json({ error: 'Error de servidor' });
+        } else {
+            if (resultado.length === 0) {
+                // Las credenciales no coinciden
+                console.error('Credenciales incorrectas');
+                res.render('login.ejs', { credencialesIncorrectas: true });
+                /* Esta es la condicional para el error de credenciales, su implementacion es igual para todos los errores que requieran alerta, solo es cambiar la variable y el if dentr del archivo ejs que quieras introducir el alert */
+            } else {
+                // Credenciales válidas, el usuario ha iniciado sesión correctamente
+                console.log('Inicio de sesión exitoso');
+                res.redirect('/homeu');
+            }
+        }
+    });
+});
 
 module.exports = router;
